b2311bf3114170e59cc5babc45b2fb5668d7d65c
Abhishek Chaturvedi <abhishekchaturvedi419@gmail.com>
2021-02-11 18:56:15 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,39 @@
+import React from "react"
+import TodoItem from "./TodoItem"
+import todosData from "./todosData"
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            todos: todosData
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+    
+    handleChange(id) {
+        this.setState(prev => {
+            const updated = prev.todos.map(item => {
+                if(item.id === id){
+                    item.completed = !item.completed
+                }
+                return item
+            })
+            return {
+                todos : updated
+            }
+        })
+    }
+    
+    render() {
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange = {this.handleChange}/>)
+        
+        return (
+            <div className="todo-list">
+                {todoItems}
+            </div>
+        )    
+    }
+}
+
+export default App
